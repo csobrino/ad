5342f6a92bac080d2c6f4d2dfdd5eab3189b1818
csobrino <dam.csobrino@institutoserpis.org>
2016-09-20 10:06:28 +0200

    Añadido funcion Nuevo y DataReader

--- dbprueba/PDbPrueba/PDbPrueba/Program.cs
+++ dbprueba/PDbPrueba/PDbPrueba/Program.cs
@@ -1,12 +1,109 @@
 using System;
+using System.Data;
+using MySql.Data.MySqlClient;
 
-namespace PDbPrueba
+
+namespace PdbPrueba
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+
+
+
+
+
+			Console.WriteLine ("Probando Acceso a dbprueba");
+
+
+
+			//MySqlConnection mySqlConnection = new MySqlConnection (
+			IDbConnection dbConnection = new MySqlConnection (	
+			"Database=dbprueba;User Id=root;Password=SISTEMAS"
+				);
+			dbConnection.Open ();
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
+		
+	
+
+
+
+
+
+
+			//mySqlConnection.CreateCommand().
+
+
+			do {
+
+				Console.Write ("0) Salir" + "\n");
+				Console.Write ("1) Nuevo" + "\n");
+				Console.Write ("2) Editar" +"\n");
+				Console.Write ("3) Eliminar" +"\n");
+				Console.Write ("4º) Listar Todos" +"\n");
+
+				switch (Console.Read ()) {
+				case '0':
+					Console.Clear ();
+					Console.Write ("\n" + "La conexion ha finalizado");
+			
+					dbConnection.Close ();
+					dbConnection = null;
+					break;
+
+
+
+				case '1':
+					Console.Write ("Seleccionó Nuevo");
+
+
+					dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+					dbDataParameter.ParameterName = "nombre";
+					dbDataParameter.Value = "Categoria 4";
+					dbCommand.Parameters.Add (dbDataParameter);
+					dbCommand.ExecuteNonQuery ();
+
+				// Continuar lógica y extraer métodos //
+					break;
+
+				case '2':
+					Console.Clear ();
+					Console.Write ("Editar");
+
+				// Continuar lógica y extraer métodos //
+					break;
+
+				case '3':
+					Console.Write ("Eliminar");
+				// Continuar lógica y extraer métodos //
+					break;
+
+
+				case '4':
+					Console.Clear ();
+
+					Console.Write ( "----------------------------"+ "\n"+"Seleccionó Listar" +"\n" + "----------------------------"+ "\n");
+					dbCommand.CommandText = "SELECT * FROM categoria";
+					IDataReader dataReader = dbCommand.ExecuteReader ();	
+
+
+				
+					while (dataReader.Read()) {
+						Console.WriteLine ("Id" + dataReader ["id"] + "\t Nombre: " + dataReader ["nombre"] +"\n");
+					}
+					dataReader.Close ();
+
+					break;
+				}
+
+			} while(dbConnection != null);
+		}
+
+			//operaciones...
+
+
 		}
 	}
-}
+
