606cdaeb0059d903853260f39877323d7330aa27
csobrino <dam.csobrino@institutoserpis.org>
2016-10-18 09:26:05 +0200

    lista articuloy datareader englobado en un metodo, Añadido sensitive a los botones.

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -21,6 +21,25 @@ public partial class MainWindow: Gtk.Window
 			);
 		dbConnection.Open ();
 
+
+		fill ();
+
+	
+		treeView.Selection.Changed += delegate {
+			bool selected = treeView.Selection.CountSelectedRows() > 0;
+			editAction.Sensitive = selected;
+			deleteAction.Sensitive = selected;
+			Console.WriteLine ("treeView.Selection.Changed selected ={0}", selected);
+		};
+
+		refreshAction.Activated += delegate {
+			fill();
+		
+		};
+	}
+
+	protected void fill(){
+
 		List<Articulo>list =  new List<Articulo>();
 		//TODO Rellenar desde la tabla articulo
 		string selectSql = "Select * from articulo";
@@ -28,25 +47,22 @@ public partial class MainWindow: Gtk.Window
 		dbCommand.CommandText = selectSql;
 		IDataReader dataReader = dbCommand.ExecuteReader ();
 
+
 		while (dataReader.Read()) {
 
 			long id = (long)dataReader ["id"]; //Al poner (long) hacemos una conversion FORZADA
 			string nombre = (string)dataReader ["nombre"];
 			decimal? precio = dataReader ["precio"] is DBNull ? null : (decimal?)dataReader["precio"];
 			long? categoria = dataReader ["categoria"] is DBNull ? null : (long?)dataReader ["categoria"];
-				Articulo articulo = new Articulo(id, nombre, precio, categoria);
+			Articulo articulo = new Articulo(id, nombre, precio, categoria);
 			list.Add (articulo);
 		}
 		dataReader.Close ();
 
-
-		//long mylong = null; (error)
-		//NO Se pueden convertir tipos "valor" a null
-		//mediante el "?" definimos que es tipo "nulable"
-
-
-
+		editAction.Sensitive = false;
+		deleteAction.Sensitive = false;
 		TreeViewHelper.Fill (treeView, list);
+	
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -56,3 +72,12 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 }
+
+
+
+
+
+//APUNTES.
+//long mylong = null; (error)
+//NO Se pueden convertir tipos "valor" a null
+//mediante el "?" definimos que es tipo "nulable"
